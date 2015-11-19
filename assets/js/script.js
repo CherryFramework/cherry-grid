642e759b677f50d4ecab7f8dd3c5549d104a3e4e
mjhead <andremjhead@gmail.com>
2015-11-19 17:21:50 +0200

    FIX: JS code style guide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-(function($) {
+(function( $ ) {
 
-	$(function(){
+	$(function() {
 
 		if ( $.isFunction( jQuery.fn.masonry ) ) {
-			var $container = $('.cherry-grid.type-masonry .cherry-grid_list');
+			var $container = $( '.cherry-grid.type-masonry .cherry-grid_list' );
 			$container.masonry( $container.data( 'masonry-atts' ) );
 		}
 
